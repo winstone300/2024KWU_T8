db2cbd1e568f5c204efa73b3352e6c0a0e919351
C0mput3-tech <rlagocks2638@gmail.com>
2024-06-17 14:41:21 +0900

    블록 색 및 길이 변경

--- oss_rythm/oss_rythm/Form1.Designer.cs
+++ oss_rythm/oss_rythm/Form1.Designer.cs
@@ -148,7 +148,7 @@
             this.label1.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label1.Location = new System.Drawing.Point(558, 117);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 29);
+            this.label1.Size = new System.Drawing.Size(120, 37);
             this.label1.TabIndex = 13;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -156,17 +156,19 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
             this.label2.Font = new System.Drawing.Font("굴림", 33F);
             this.label2.Location = new System.Drawing.Point(555, 234);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(141, 44);
+            this.label2.Size = new System.Drawing.Size(177, 55);
             this.label2.TabIndex = 14;
             this.label2.Text = "label2";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(800, 716);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
